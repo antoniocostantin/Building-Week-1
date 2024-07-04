03da26c84cebd7ec4c9d38c1bfd924dd00538160
oBeta97 <136509239+oBeta97@users.noreply.github.com>
2024-07-04 16:37:22 +0200

    js pagina 4

---
+++ assets/js/scriptPg4.js
@@ -0,0 +1,14 @@
+const feedbackInput = document.querySelector('#feedback');
+const ratingsForm = document.getElementById('ratingsForm')
+
+window.addEventListener('load', function () {
+
+    ratingsForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const selectedStar = document.querySelector('input[name="rating"]:checked');
+        alert(`
+        Valutazione: ${selectedStar.value} stelle ${selectedStar.value == 5 ? 'san montana' : ''} \n 
+        Commento: ${feedbackInput.value}    
+    `)
+    });
+});
